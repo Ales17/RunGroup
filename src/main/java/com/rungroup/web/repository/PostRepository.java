e887cd44c8c151c80cad2c5e0160752c6c37f872
Ales Bacul <alesbacul@gmail.com>
2024-08-09 10:07:33 +0200

    create post repo

---
+++ src/main/java/com/rungroup/web/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.rungroup.web.repository;
+
+import com.rungroup.web.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
