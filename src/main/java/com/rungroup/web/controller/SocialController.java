ff539a016cc7db3e6024a7f7c4dd85c51b3b62ea
Ales Bacul <alesbacul@gmail.com>
2024-08-11 08:53:57 +0200

    complete basic posting feature

--- src/main/java/com/rungroup/web/controller/SocialController.java
+++ src/main/java/com/rungroup/web/controller/SocialController.java
@@ -1,16 +1,14 @@
 package com.rungroup.web.controller;
 
 import com.rungroup.web.dto.PostDto;
+import com.rungroup.web.models.Post;
 import com.rungroup.web.models.UserEntity;
 import com.rungroup.web.security.SecurityUtil;
 import com.rungroup.web.service.SocialService;
 import com.rungroup.web.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,7 +38,25 @@ public class SocialController {
         return "social";
     }
 
+    @GetMapping("posts")
+    public String getPosts(Model model) {
+        List<PostDto> posts = socialService.findAllPosts();
+        model.addAttribute("posts", posts);
+        return "fragments/social::posts";
+    }
 
+    @GetMapping("post/new")
+    public String newPostForm(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "fragments/social::post-new";
+    }
+
+    @PostMapping("post/new")
+    public String createPost(@ModelAttribute PostDto dto) {
+        socialService.createPost(dto);
+        return "redirect:/posts";
+    }
     @GetMapping("/post/{postId}/status")
     public String getUserLikeStatus(@PathVariable("postId") long postId) {
         if (socialService.isPostLikedBySessionUser(postId))
